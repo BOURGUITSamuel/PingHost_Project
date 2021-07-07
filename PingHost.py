16e24a662d4149764c186c364b282fb8db16eda5
BOURGUITSamuel <samuel.bourguit@gmail.com>
2021-07-07 19:57:49 +0200

    Initial commit

---
+++ PingHost.py
@@ -0,0 +1,55 @@
+# coding: utf-8
+
+import os
+import sys
+import colorama
+from colorama import Fore
+
+# Verifies your os type
+OS_TYPE = os.name
+
+# Sets the count modifier to the os type
+count = '-n' if OS_TYPE == 'nt' else '-c'
+
+
+def create_ip_list():
+    """Creates an ip address list"""
+    ip_list = []
+
+    with open("ip_file.txt", "r") as file:
+        for line in file:
+            ip_list.append(line.strip())
+    return ip_list
+
+
+def ping_device(ip_list):
+    """Ping ip_list and return results"""
+    results_file = open("results.txt", "w")
+
+    for ip in ip_list:
+        response = os.popen(f"ping {ip} {count} 1").read()
+        if "TTL" in response:
+            print(Fore.GREEN + f'UP' + Fore.WHITE + '', f'[{ip}]' + '', 'Ping Successful')
+            results_file.write(f"UP {ip} Ping Successful" + "\n")
+        else:
+            print(Fore.RED + f'DOWN' + Fore.WHITE + '', f'[{ip}]' + '', 'Ping UnSuccessful')
+            results_file.write(f"Down {ip} Ping Unsuccessful" + "\n")
+    results_file.close()
+
+
+def main():
+    """ Verify if IP list is empty before starting IP Scan"""
+
+    filesize = os.path.getsize("ip_file.txt")
+
+    if filesize == 0:
+        print(Fore.RED + "The program has been interrupted : The IP list is empty!")
+        sys.exit()
+    else:
+        print('Start IP Scan...')
+        ping_device(create_ip_list())
+        print('IP Scan is finish!')
+
+
+if __name__ == "__main__":
+    main()
---
+++ Readme.md
@@ -0,0 +1,37 @@
+# PingHost.py
+Ce script ping un périphérique réseau pour vérifier si le périphérique est actif ou inactif
+
+Vous pouvez aussi effectuer un test ping avec un hôte  ou nom de domaine
+## Getting Started
+Test et diagnostic du réseau  à l'aide de la commande  Ping
+
+# Prerequisites
+Le programme peut être exécuté  sur un système d'exploitation Windows(en utilisant un IDE) ou Linux
+
+Le programme a été conçu avec le langage Python
+
+le module python 'colorama' est requis : https://pypi.org/project/colorama/
+
+## Installing & Using
+1- Copiez le fichier dans le répertoire de votre choix
+
+2- Lancez le programme avec la commande suivante : python PingHost.py
+
+3- Vous pouvez appliquer vos propres paramètres en modifiant le script
+
+## Running the tests
+Le programme a été conçu dans Un environnement de développement intégré(IDE) sur l'OS Windows 10 64Bits.
+
+Le programme a été testé sur l'OS Debian 64bits.
+
+## Versioning
+Version 1.0 
+
+## Authors
+Jean - Samuel BOURGUIT 
+Administrateur Infrastuture et Cloud
+## License
+    Apache License
+    Version 2.0, January 2004
+    http://www.apache.org/licenses/
+https://www.apache.org/licenses/LICENSE-2.0.txt
